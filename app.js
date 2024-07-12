718b79221afb0c0cdbf84ce7975aa2c6e0df2052
EzeMatthew <ezebongmathew5@gmail.com>
2024-07-13 00:04:41 +0100

    Cart_project

--- app.js
+++ app.js
@@ -1,12 +1,2 @@
-const btn = document.querySelectorAll('.btn')
-const header4 = document.querySelectorAll('h4')
-
-btn.forEach(btn => {
-    btn.addEventListener('click ',() => {
-        console.log(`btn ${btn.tagName} clicked`)
-        if(btn.textContent === 'Add to Cart')
-            console.log('you have successfully added')
-    })
-})
 
 
